3dd5ff4bc433a90d7030c83c128dbb68b9f71cb8
Kernela <112568330+KernelaKlees@users.noreply.github.com>
2026-06-07 18:34:33 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Codey";
+let botLocation = "JavaScript Island";
+let favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `${favoriteLanguage} was created in just 10 days!`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is used to build both websites and servers.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is the most popular programming language in the world.`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
